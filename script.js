3727870c3515e4d2c723bcc5f669eb0dc234c35b
Serhiy <serhiykarpiak1@gmail.com>
2023-05-12 09:09:20 +0300

    Завершив роботу над сторінкою

--- script.js
+++ script.js
@@ -1,23 +1,65 @@
-
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.header-swiper', {
     loop: true,
     autoplay: {
         delay: 3000,
     },
     speed: 800,
     effect: 'coverflow',
-    // easing: 'linear',
 })
 
-
+const feedbackSwiper = new Swiper('.feedback-swiper', {
+    loop: true,
+    effect: 'flip',
+    speed: 600,
+    keyboard: {
+        enabled: true
+    },
+    navigation: {
+        nextEl: '.right-button',
+        prevEl: '.left-button',
+    },
+})
 
 const burgerMenuIcon = document.querySelector('.burger-menu')
 const burgerMenu = document.querySelector('.fullscreen-menu')
 
-burgerMenuIcon.addEventListener('click', function(){
+burgerMenuIcon.addEventListener('click', function () {
     burgerMenu.classList.toggle('active-menu')
 })
 
 
 
-AOS.init()
+AOS.init({
+    once: true
+})
+
+
+
+let runningText = document.querySelectorAll('.heading-move')
+let position = 0
+
+function animate() {
+    position += 1
+
+    runningText.forEach((text, index) => {
+        if (index === 1) {
+            text.style.transform = `translateX(-${position}px)`
+        } else {
+            text.style.transform = `translateX(${position}px)`
+        }
+
+        if (position > text.offsetWidth) {
+            position = 0
+        }
+    })
+
+    requestAnimationFrame(animate)
+}
+
+animate()
+
+
+
+
+
+
